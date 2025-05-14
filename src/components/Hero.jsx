47f4024df60648c83337e08ee268bbe2d8376686
erdipratama666 <165442285+erdipratama666@users.noreply.github.com>
2025-05-14 23:50:17 +0700

    Update semua perubahan terbaru

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import '../styles/Hero.css';
-import avatar from '../assets/profile-picture.png';
+import avatar from '/assets/profile-picture.png'; // Import gambar dengan benar
 
 const Hero = () => {
   return (
@@ -12,7 +12,14 @@ const Hero = () => {
       </div>
       
       <div className="hero-img">
-        <img src={avatar} alt="Erdi Pratama" />
+        {/* Gunakan atribut loading="lazy" dan ukuran yang optimal */}
+        <img 
+          src={avatar} 
+          alt="Erdi Pratama"
+          loading="lazy"
+          width="400" 
+          height="400" 
+        />
       </div>
     </section>
   );
