fecacf66c5b8babe526a9afc85ee1ffa134f1a3e
erdipratama666 <165442285+erdipratama666@users.noreply.github.com>
2025-05-15 20:28:34 +0700

    Update semua proyek

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import './App.css'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <Home />
-  )
+    <HelmetProvider>
+      <Helmet>
+        <title>Erdi Pratama | Web Developer</title>
+        <meta name="description" content="Portofolio Erdi Pratama, Web Developer dengan React.js" />
+      </Helmet>
+      < Home />
+    </HelmetProvider>
+  );
 }
 
-export default App
+export default App;
