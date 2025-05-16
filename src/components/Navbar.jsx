f12a42cebfad702e978782d842167032d9b156a1
erdipratama666 <165442285+erdipratama666@users.noreply.github.com>
2025-05-16 13:16:18 +0700

    Update project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Navbar.css';
+
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
