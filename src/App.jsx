fcc9cdffae3b2eefaadbd8e7c4ccc7bad0420ee5
erdipratama666 <165442285+erdipratama666@users.noreply.github.com>
2025-05-16 10:52:19 +0700

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { HelmetProvider, Helmet } from 'react-helmet-async';
-import './App.css';
-// Import Home secara normal karena ini adalah komponen utama yang menampilkan konten LCP
-import Home from './pages/Home';
+import './App.css'
+import Home from './pages/Home'
 
 function App() {
   return (
@@ -9,31 +8,8 @@ function App() {
       <Helmet>
         <title>Erdi Pratama | Web Developer</title>
         <meta name="description" content="Portofolio Erdi Pratama, Web Developer dengan React.js" />
-        {/* Preload font jika Anda menggunakan custom font */}
-        {/* <link rel="preload" href="/fonts/your-font.woff2" as="font" type="font/woff2" crossOrigin="anonymous" /> */}
-        
-        {/* Preconnect ke domain penting jika ada */}
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        
-        {/* Inline critical CSS jika memungkinkan */}
-        <style type="text/css">{`
-.hero {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10rem 8rem 15rem;
-  background-color: #000000;
-  color: white;
-  flex-wrap: wrap;
-  gap: 2rem;
-}
-
-.hero-text {
-  max-width: 600px;
-}
-        `}</style>
       </Helmet>
-      <Home />
+      < Home />
     </HelmetProvider>
   );
 }
