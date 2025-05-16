728be4c1abd1134311c0e61f2ae629a1dd838241
erdipratama666 <165442285+erdipratama666@users.noreply.github.com>
2025-05-16 09:25:30 +0700

    Update project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,34 @@
 import '../styles/Hero.css';
+import { Helmet } from 'react-helmet';
 import avatar from '../assets/profile-picture.png';
 
 const Hero = () => {
   return (
-    <section className="hero" data-testid="hero-section">
-      <div className="hero-text">
-        <p className="intro">Hi, I'm</p>
-        <h1 itemProp="name">Erdi Pratama</h1>
-        <h2 itemProp="jobTitle">Web Developer</h2>
-        <p className="welcome">Welcome to My personal website.</p>
-      </div>
-      
-      <div className="hero-img">
-        <img 
-          src={avatar} 
-          alt="Erdi Pratama - Web Developer" 
-          width="300" 
-          height="300" 
-          loading="eager"
-          itemProp="image"
-        />
-      </div>
-    </section>
+    <>
+      <Helmet>
+        <link rel="preload" href={avatar} as="image" />
+      </Helmet>
+
+      <section className="hero" data-testid="hero-section">
+        <div className="hero-text">
+          <p className="intro">Hi, I'm</p>
+          <h1 itemProp="name">Erdi Pratama</h1>
+          <h2 itemProp="jobTitle">Web Developer</h2>
+          <p className="welcome">Welcome to My personal website.</p>
+        </div>
+
+        <div className="hero-img">
+          <img
+            src={avatar}
+            alt="Erdi Pratama - Web Developer"
+            width="300"
+            height="300"
+            loading="eager"
+            itemProp="image"
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
