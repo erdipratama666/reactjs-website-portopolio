f3caec860235b7b64528f5a41bdb8bff5ae57604
erdipratama666 <165442285+erdipratama666@users.noreply.github.com>
2025-05-19 13:01:21 +0700

    eer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -51,15 +51,7 @@ function About() {
               <h1 itemProp="name">Erdi Pratama</h1>
               <h2>About Me</h2>
               <p itemProp="description">
-                Hai, saya <span itemProp="name">Erdi Pratama</span>!
-                <br /><br />
-                <span itemProp="jobTitle">Mahasiswa & Web Developer</span> yang sedang menekuni bidang pengembangan web (web development).
-                Saat ini, saya aktif mempelajari berbagai teknologi seperti <span itemProp="knowsAbout">HTML, CSS, JavaScript</span>,
-                dan framework modern untuk membangun website yang fungsional dan menarik.
-                <br /><br />
-                Saya sangat tertarik dengan dunia pemrograman dan selalu bersemangat untuk mengasah
-                kemampuan coding saya. Selain kuliah, saya juga mengerjakan proyek kecil-kecilan
-                untuk memperdalam pemahaman tentang front-end dan back-end development.
+                Hai, saya <span itemProp="name">Erdi Pratama</span>! <span itemProp="jobTitle">Mahasiswa & Web Developer</span> yang sedang menekuni bidang pengembangan web (web development). Saat ini, saya aktif mempelajari berbagai teknologi seperti <span itemProp="knowsAbout">HTML, CSS, JavaScript</span>, dan framework modern untuk membangun website yang fungsional dan menarik. Saya sangat tertarik dengan dunia pemrograman dan selalu bersemangat untuk mengasah kemampuan coding saya. Selain kuliah, saya juga mengerjakan proyek kecil-kecilan untuk memperdalam pemahaman tentang front-end dan back-end development.
               </p>
               {socialLinks}
             </div>
